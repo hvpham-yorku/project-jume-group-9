0dd8ff60b78e1ed20b080e072b7dc1f733a33c50
Jay Patel <jayrpatelca@gmail.com>
2025-04-03 17:24:57 -0400

    Added update order fulfillment status to actions

--- app/(app)/orgs/[orgId]/(sidebar)/orders/_actions/index.ts
+++ app/(app)/orgs/[orgId]/(sidebar)/orders/_actions/index.ts
@@ -6,6 +6,73 @@ import { Database } from "@/utils/supabase/types";
 import { revalidateTag, unstable_noStore } from "next/cache";
 
 /* -------------------------------------------------------------------------- */
+/*                        updateOrdersFulfillmentStatus                       */
+/* -------------------------------------------------------------------------- */
+interface UpdateOrdersFulfillmentStatusProps {
+  orgId: string;
+  orderIds: string[];
+  fulfillment_status: Database["public"]["Enums"]["fulfillment_status"];
+}
+export const updateOrdersFulfillmentStatus = async ({
+  orgId,
+  orderIds,
+  fulfillment_status,
+}: UpdateOrdersFulfillmentStatusProps) => {
+  unstable_noStore();
+
+  const supabase = await createClient();
+
+  const { data: orders } = await supabase
+    .from("orders")
+    .update({ fulfillment_status })
+    .in("id", orderIds)
+    .eq("org_id", orgId)
+    .select("*")
+    .throwOnError();
+
+  revalidateTag("orders");
+
+  await addActivityLog({
+    orgId: orgId,
+    action: `Orders: Update Fulfillment Status To ${fulfillment_status.toUpperCase()}`,
+    description: `Order fulfillment status updated to ${fulfillment_status} for ${orders.length} orders. List of orders: ${orders.map((order) => order.order_number).join(", ")}`,
+  });
+
+  return orders;
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                deleteOrders                                */
+/* -------------------------------------------------------------------------- */
+interface DeleteOrdersProps {
+  orgId: string;
+  orderIds: string[];
+}
+export const deleteOrders = async ({ orgId, orderIds }: DeleteOrdersProps) => {
+  unstable_noStore();
+
+  const supabase = await createClient();
+
+  const { data: orders } = await supabase
+    .from("orders")
+    .delete()
+    .in("id", orderIds)
+    .eq("org_id", orgId)
+    .select("*")
+    .throwOnError();
+
+  revalidateTag("orders");
+
+  await addActivityLog({
+    orgId: orgId,
+    action: "Orders: Delete",
+    description: `Orders deleted. List of orders: ${orders.map((order) => order.order_number).join(", ")}`,
+  });
+
+  return orders;
+};
+
+/* -------------------------------------------------------------------------- */
 /*                                 createOrder                                */
 /* -------------------------------------------------------------------------- */
 interface CreateOrderProps {
@@ -61,35 +128,4 @@ export const updateOrder = async ({ orgId, orderId, order }: UpdateOrderProps) =
   });
 
   return updatedOrder;
-};
-
-/* -------------------------------------------------------------------------- */
-/*                                deleteOrders                                */
-/* -------------------------------------------------------------------------- */
-interface DeleteOrdersProps {
-  orgId: string;
-  orderIds: string[];
-}
-export const deleteOrders = async ({ orgId, orderIds }: DeleteOrdersProps) => {
-  unstable_noStore();
-
-  const supabase = await createClient();
-
-  const { data: orders } = await supabase
-    .from("orders")
-    .delete()
-    .in("id", orderIds)
-    .eq("org_id", orgId)
-    .select("*")
-    .throwOnError();
-
-  revalidateTag("orders");
-
-  await addActivityLog({
-    orgId: orgId,
-    action: "Orders: Delete",
-    description: `Orders deleted. List of orders: ${orders.map((order) => order.order_number).join(", ")}`,
-  });
-
-  return orders;
 };

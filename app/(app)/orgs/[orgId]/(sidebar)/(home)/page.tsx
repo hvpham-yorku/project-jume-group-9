65a1e017cfe3215c10ed547f67ee5215ae2e4690
Max <max@maxs-mbp.home>
2025-04-03 20:27:21 -0400

    Created home dashboard that shows the a summary of all data including  notifications

--- app/(app)/orgs/[orgId]/(sidebar)/(home)/page.tsx
+++ app/(app)/orgs/[orgId]/(sidebar)/(home)/page.tsx
@@ -1,7 +1,16 @@
+import HomeDashboard from "./_components/home-dashboard";
+import { createClient } from "@/utils/supabase/client/server";
+import { getDashboardData } from "./_data";
+import { getOrgMember } from "@/app/data";
 
 interface HomePageProps {
   params: Promise<{ orgId: string }>;
 }
 export default async function HomePage({ params }: HomePageProps) {
-  return <div className="size-full flex items-center justify-center p-10">Home Page [Under Construction]</div>;
+  const { orgId } = await params;
+  const supabase = await createClient();
+
+  const [orgMember, initialData] = await Promise.all([getOrgMember({ orgId }), getDashboardData({ supabase, orgId })]);
+
+  return <HomeDashboard orgId={orgId} orgMember={orgMember} initialData={initialData} />;
 }
